83b4109a54dc177321072607e8b441638d4447f5
Omardyab <omarzadyab@gmail.com>
2021-04-22 06:38:01 +0300

    first problem is solved

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
+  let z=a+b;
+  return [z, ' the sum of '+a+'and '+b+' is '+z];
 }
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+console.log(sum(4,7));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
